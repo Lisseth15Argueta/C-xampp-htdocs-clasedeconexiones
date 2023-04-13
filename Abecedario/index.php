cf8d1f1afaabf6bd7b9879d7f4972b3928725f7c
LissethBenitez <129644993+LissethBenitez@users.noreply.github.com>
2023-04-13 13:22:07 -0600

    Add files via upload
    Ejercicios de PHP

---
+++ Abecedario/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width-device-width, initial-scale-1.0">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+   integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/iJfQUOhcWr7x9JvoRxT2MZw1T" crossorigin=""anonymous>
+    <title>Aprender</title>
+    <script>
+       
+    </script>
+</head>
+<body>
+
+<nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="./img/287709-aprender-mas-rapido.webp" width="30" height="30" class="d-inline-blok align-top" alt="">
+        Aprender la Abecedario
+    </a>
+</nav>
+
+<div class="jumbotron">
+    <form action="index.php">
+<div class="box">
+    <div class="card" style="width: 16rem; height: 14rem;">
+    <img class="card-img-top" src="./img/primera abecedario.png" alt="Card image cap" style="width: 15rem; height:13rem;">
+        <div class="card-body">
+        <h5 class="card-title">Abecedario</h5>
+        <input type="submit" class="btn btn-primary" name="abecedario1" value="Reproducir">
+        
+</div>
+    </div>
+        </div>
+
+<div class="box">
+    <div class="card" style="width: 16rem; height: 14rem;">
+    <img class="card-img-top" src="./img/segunda abecedarios.png" alt="Card image cap" style="width: 15rem; height:13rem;">
+        <div class="card-body">
+        <h5 class="card-title">Abecedario</h5>
+        <input type="submit" class="btn btn-primary" name="abecedario2" value="Reproducir">
+</div>
+    </div>
+        </div>
+
+<div class="box">
+    <div class="card" style="width: 16rem; height: 14rem;">
+    <img class="card-img-top" src="./img/tercera abecedarios.png" alt="Card image cap" style="width: 15rem; height:13rem;">
+        <div class="card-body">
+        <h5 class="card-title">Abecedario</h5>
+        <input type="submit" class="btn btn-primary" name="abecedario3" value="Reproducir">
+</div>
+    </div>
+        </div>
+    </div>
+    </from>
+</body>
+</html>
+<?php
+include_once('letras.php');
+?>
+    
+</body>
+</html>
